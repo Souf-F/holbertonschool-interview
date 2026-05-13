033f3f9c430321b6af7a318c26d8fda032cd5a2c
Souf-F <sfecom.31000@gmail.com>
2026-05-13 09:14:49 +0200

    style: format code by removing unnecessary whitespace

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -7,25 +7,25 @@ Pascal's Triangle module
 def pascal_triangle(n):
     """
     Returns a list of lists representing Pascal's triangle of n.
-    
+
     Args:
         n (int): Number of rows
-    
+
     Returns:
         list: List of lists of integers, empty list if n <= 0
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
-    
+
     for i in range(1, n):
         row = [1]
-        
+
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        
+
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
