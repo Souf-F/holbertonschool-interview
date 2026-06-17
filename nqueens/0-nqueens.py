9066919760db0c9a1dd5f2a358eff5759328b4ed
Souf-F <sfecom.31000@gmail.com>
2026-06-17 10:35:00 +0200

    refactor: streamline N-Queens solution by consolidating functions and improving readability

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-
+"""
+the N Queen program
+"""
 import sys
 
 
-def is_safe(board, row, col, n):
-    for i in range(row):
-        if board[i] == col:
+def create_board(n):
+    """initiate chess board"""
+    return []
+
+
+def safe_square(row, col, board):
+    """check if a queen can be placed"""
+    for i, j in board:
+        if j == col:
             return False
-        if abs(board[i] - col) == abs(i - row):
+        if abs(i - row) == abs(j - col):  # column and diagonal check
             return False
     return True
 
 
-def solve_nqueens(board, row, n, solutions):
+def backtracking(n, row, board, result):
+    """recursive way to find the right path"""
     if row == n:
-        solution = [[i, board[i]] for i in range(n)]
-        solutions.append(solution)
+        result.append(list(board))
         return
 
     for col in range(n):
-        if is_safe(board, row, col, n):
-            board[row] = col
-            solve_nqueens(board, row + 1, n, solutions)
-
-
-def nqueens(n):
-    board = [-1] * n
-    solutions = []
-    solve_nqueens(board, 0, n, solutions)
-    return solutions
+        if safe_square(row, col, board):
+            board.append([row, col])
+            backtracking(n, row + 1, board, result)
+            board.pop()  # The Backtrack
 
 
 def main():
+    """block handling"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
@@ -46,9 +49,12 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
 
-    solutions = nqueens(n)
-    for solution in solutions:
-        print(solution)
+    result = []
+    board = create_board(n)
+    backtracking(n, 0, board, result)
+
+    for results in result:
+        print(results)
 
 
 if __name__ == "__main__":
